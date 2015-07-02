8b16dc6196c53ae58c9bfe5bf6891a6a1c8d0539
Ryan Yogan <ryanyogan@gmail.com>
2015-07-02 13:39:48 -0500

    Added a sample iterrator

--- js/shouter.js
+++ js/shouter.js
@@ -4,12 +4,31 @@ const {
   Component,
 } = React;
 
+const list = [
+  'Toothbrush',
+  'Cheesy Poofs',
+  'Beer'
+];
+
+class GroceryList extends Component {
+  render () {
+    return (
+      <ul>
+        {list.map((item, index) => {
+          return <li key={index}>{item}</li>;
+        })}
+      </ul>
+    );
+  }
+};
+
 class Shouter extends Component {
   render () {
     return (
       <div>
         <p>Hello {this.props.name} !</p>
         <span className="age">You are {this.props.age} years old</span>
+        <GroceryList />
       </div>
     );
   }
