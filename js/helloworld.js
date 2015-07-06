a446bfaa8dc3c85a7f5a75099a5e1767a0f3a492
Ryan Yogan <ryanyogan@gmail.com>
2015-07-06 13:28:46 -0500

    Added Material UI pre-reqs

--- js/helloworld.js
+++ js/helloworld.js
@@ -1,6 +1,10 @@
 var ShouterForm = require('./shouterForm');
 var React       = require('react');
 
+var injectTapEventPlugin = require('react-tap-event-plugin');
+
 window.React = React;
+
+injectTapEventPlugin();
 
 React.render(<ShouterForm />, document.getElementById('application'));
