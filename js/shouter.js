c18b54ec678b8efb08f43df25716d91ba14bf352
Ryan Yogan <ryanyogan@gmail.com>
2015-07-06 11:07:45 -0500

    Change name to Galaxy

--- js/shouter.js
+++ js/shouter.js
@@ -26,7 +26,7 @@ class Shouter extends Component {
   render () {
     return (
       <div>
-        <p>Hello {this.props.name} !</p>
+        <p>Hello {this.props.name}!</p>
         <span className="age">You are {this.props.age} years old</span>
         <GroceryList />
       </div>
